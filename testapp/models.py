8ef31e88551bd18eaa070c89ac3830db522228ff
Eric Weger <ericweger@gmail.com>
2017-01-20 00:20:00 -0800

    basic form handling

--- testapp/models.py
+++ testapp/models.py
@@ -4,9 +4,12 @@ from django.db import models
 
 # Create your models here.
 class TestContent(models.Model):
+	id = models.AutoField(primary_key=True)
 	content_text = models.TextField()
 	content_name = models.CharField(max_length=50, blank=True)
+	def __str__(self):
+		return self.content_name
 	def save(self, *args, **kwargs):
 		if not self.content_name:
-			self.content_name = "Section"+self.id
+			self.content_name = "Section "+str(self.id)
 		super(TestContent, self).save(*args, **kwargs)
