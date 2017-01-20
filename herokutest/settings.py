b326e946fcd45c533de43edec885a643d9322d32
Eric Weger <ericweger@gmail.com>
2017-01-19 18:30:59 -0800

    update template variables

--- herokutest/settings.py
+++ herokutest/settings.py
@@ -24,7 +24,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = "-(%jm!kbefw-adsx*&09^0l*5#&aiq34)nrw3pm&0@5fpf=9!#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Application definition
 
