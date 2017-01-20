3b7d639a34cc31dc75410e5fe76b26a88eaa475b
Eric Weger <ericweger@gmail.com>
2017-01-19 18:20:15 -0800

    template inheritance and content_name

--- testapp/models.py
+++ testapp/models.py
@@ -5,3 +5,8 @@ from django.db import models
 # Create your models here.
 class TestContent(models.Model):
 	content_text = models.TextField()
+	content_name = models.CharField(max_length=50, blank=True)
+	def save(self, *args, **kwargs):
+		if not self.content_name:
+			self.content_name = "Section"+self.id
+		super(TestContent, self).save(*args, **kwargs)
